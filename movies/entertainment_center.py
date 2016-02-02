7330ea216e8b17d7e620479adada1745c9d3466c
Hao Wu <tohaowu@gmail.com>
2016-02-02 09:08:54 -0800

    useless

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -29,4 +29,6 @@ hunger_games = media.Movie("Hunger Games", "Storyline",
                     "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-fresh_tomatoes.open_movies_page(movies)
+#fresh_tomatoes.open_movies_page(movies)
+#print media.Movie.VALID_RATINGS
+print media.Movie.__doc__
